b92c3cd215270c1350461c191c6d44e39e1ecdde
Keerty Verma <keertyverma1303@gmail.com>
2023-05-04 23:02:18 +0530

    Display genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={10} padding={10}>
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} spacing={3} padding={3}>
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer key={skeleton}>

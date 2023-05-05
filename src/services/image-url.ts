632cea6867a5aa5d6ff2f239181657e4551da9f7
Keerty Verma <keertyverma1303@gmail.com>
2023-05-05 14:03:42 +0530

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,8 @@
+import noImageURL from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImageURL;
+
   const target = "io/media";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "/crop/600/400/" + url.slice(index);

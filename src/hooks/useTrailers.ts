1e9c6b36d12987d0f701be29f88f79a0f1bf404e
Keerty Verma <keertyverma1303@gmail.com>
2023-05-09 12:11:48 +0530

    Build Game Trailer

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import { Trailer } from "../entities/Trailer";
+import APIClient, { FetchResponse } from "../services/api-client";
+
+const useTrailers = (gameId: number) => {
+  const apiClient = new APIClient<Trailer>(`/games/${gameId}/movies`);
+
+  return useQuery<FetchResponse<Trailer>, Error>({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getAll,
+    staleTime: ms("24h"),
+  });
+};
+
+export default useTrailers;
